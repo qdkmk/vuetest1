2e5eb0fb4a3ebae6a3af7d07484e2e2cea90e5a7
qdmk <qdokuhon@gmail.com>
2019-07-08 17:28:03 +0900

    App.vue 一旦テスト用に上部のリンク（Home|About）のAboutをsysid固定の詳細画面（About.vue）へのリンクとして設定している。
    home.vue
    「読書」の検索結果を表示。各結果には詳細ページへのリンクを設定。詳細ページはabout.bueで実装。
    
    about.vue
    home.vueよりsysid(レファ協の記事の固有ID)をパラメータとして取得。sysidをもとに新たに記事内容を取得し表示。
    表示する際には42,43行目「question: question.innerHTML.replace(/\n/g,"<BR>") 
    +'\n',」で改行も置換して反映した。
    これから質問、回答以外の項目（事前調査など）も表示するように変えていく。

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,51 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h2>この記事のシステムIDは{{$route.params.sysid}}です。</h2>
+
+    <div class="columns" v-for="refqa in refqas">
+      <div class="card">
+          <div class="card-divider">
+            <span v-html="refqa.question"></span>
+          </div>
+          <div class="card-section">
+            <span v-html="refqa.answer"></span>
+          </div>
+      </div>
+    </div>
   </div>
 </template>
+
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  data() {
+    return {
+      refqas: []
+    }
+  },
+
+  mounted() {
+    axios.get("http://localhost:8000/detail?sysid=" + this.$route.params.sysid)
+      .then(response => {
+        const oParser = new DOMParser();
+        const oDOM = oParser.parseFromString(response.data, "application/xml");
+        const results = oDOM.getElementsByTagName('result');
+        for (const result of results) {
+          const question = result.getElementsByTagName("reference")[0].getElementsByTagName("question")[0];
+          const answer = result.getElementsByTagName("reference")[0].getElementsByTagName("answer")[0];
+          const id = result.getElementsByTagName("reference")[0].getElementsByTagName("sys-id")[0];
+          const obj = {
+            question: question.innerHTML.replace(/\n/g,"<BR>") +'\n',
+            answer: answer.innerHTML.replace(/\n/g,"<BR>") +'\n',
+            id: "http://crd.ndl.go.jp/reference/detail?page=ref_view&id=" + id.innerHTML
+          };
+          this.refqas.push(obj)
+        }
+      })
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,52 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="home">
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <p>{{msg}}</p>
+  <div class="columns medium-3" v-for="refqa in refqas">
+    <div class="card">
+      <router-link :to="{ name: 'about', params: { sysid: refqa.id }}" >
+        <div class="card-divider">
+          {{ refqa.question }}
+        </div>
+        <div class="card-section">
+          <p>{{ refqa.answer }}.</p>
+        </div>
+      </router-link > |
+    </div>
   </div>
+</div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      msg: 'Heello World!',
+      refqas: []
+    }
+  },
+
+  mounted() {
+    axios.get("http://localhost:8000/?keyword='読書'")
+      .then(response => {
+        const oParser = new DOMParser();
+        const oDOM = oParser.parseFromString(response.data, "application/xml");
+        const results = oDOM.getElementsByTagName('result');
+        for (const result of results) {
+          const question = result.getElementsByTagName("reference")[0].getElementsByTagName("question")[0];
+          const answer = result.getElementsByTagName("reference")[0].getElementsByTagName("answer")[0];
+          const id = result.getElementsByTagName("reference")[0].getElementsByTagName("sys-id")[0];
+          const obj = {
+            question: question.innerHTML,
+            answer: answer.innerHTML,
+            id: id.innerHTML
+          };
+          this.refqas.push(obj)
+        }
+      })
   }
 }
 </script>
