858d92f57d7d847300dae9179ae4eea1200ab7fb
qdmk <qdokuhon@gmail.com>
2019-07-08 23:30:39 +0900

    ランダム取得機能を実装。 ランダム取得後、記事をクリック、その後戻るボタンを押すと初期画面（読書の検索結果）が表示される。できれば元のランダム取得画面に戻りたい。後で修正。

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,8 +4,12 @@
   <div class="home-top">
   <h2>レファレンス事例協同データベース検索</h2>
   <div>
-    <input type="text" id="searchbox" v-model="keyword">
+    <input type="text" id="searchbox" v-model="keyword" placeholder="キーワードを入力して検索">
   </div>
+  <p>or</p>
+
+  <button class="random-button" v-on:click="randomDate">ランダムに取得</button>
+
   <p v-cloak>
     {{ message }}
   </p>
@@ -81,8 +85,41 @@ export default {
           }
         })
       this.message = '';
-    }
-  },
+    },
+
+
+  randomDate:function () {
+    const start = new Date(2004, 3, 6);
+    const end = new Date;
+    const querydate =new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+    const m = ("00" + (querydate.getMonth()+1)).slice(-2);
+    const d = ("00" + querydate.getDate()).slice(-2);
+    const regdate = querydate.getFullYear() +  m + d;
+
+    this.refqas = [];
+
+    axios.get("http://192.168.1.12:8000/random?regdate=" + regdate)
+      .then(response => {
+        const oParser = new DOMParser();
+        const oDOM = oParser.parseFromString(response.data, "application/xml");
+        const results = oDOM.getElementsByTagName('result');
+        for (const result of results) {
+          const question = result.getElementsByTagName("reference")[0].getElementsByTagName("question")[0];
+          const answer = result.getElementsByTagName("reference")[0].getElementsByTagName("answer")[0];
+          const id = result.getElementsByTagName("reference")[0].getElementsByTagName("sys-id")[0];
+          const obj = {
+            question: question.innerHTML,
+            answer: answer.innerHTML,
+            id: id.innerHTML
+          };
+          this.refqas.push(obj)
+        }
+      })
+  }
+
+
+
+},
   mounted() {
     axios.get("http://192.168.1.12:8000/?keyword='読書'")
       .then(response => {
@@ -118,6 +155,21 @@ export default {
   margin: 30px auto;
 }
 
+.random-button{
+  display: inline-block;
+  background-color: #26a69b;/*背景色*/
+  color: #FFF;/*文字色*/
+  font-size: 1em;/*文字サイズ*/
+  line-height: 1;
+  text-decoration: none;
+  letter-spacing: 0.05em;/*字間*/
+  padding: 0.5em 1em;/*ボタン内の余白*/
+  border-radius: 3px;/*角の丸み*/
+    cursor: pointer;
+  box-shadow: 0 2px 2px 0 rgba(0,0,0,0.14), 0 1px 5px 0 rgba(0,0,0,0.12), 0 3px 1px -2px rgba(0,0,0,0.2);/*影*/
+  -webkit-tap-highlight-color: transparent;
+  transition: .3s ease-out;/*変化を緩やかに*/
+}
 
 .resultswrapper{
   display: flex;
