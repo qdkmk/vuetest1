f1be9fa08b15e5c0ca2f987c9d8cc533cf7c3a3d
qdmk <qdokuhon@gmail.com>
2019-07-11 21:21:10 +0900

    vue analyticsを導入。 サイトディスクリプションの追加。

--- src/main.js
+++ src/main.js
@@ -3,8 +3,15 @@ import App from './App.vue'
 import toasted from 'vue-toasted';
 import router from './router'
 import store from './store'
+import VueAnalytics from 'vue-analytics'
 
 Vue.use(toasted)
+
+Vue.use(VueAnalytics, {
+  id:'UA-143677064-1',
+  router
+})
+
 Vue.config.productionTip = false
 
 new Vue({
